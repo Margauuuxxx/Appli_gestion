c7db0d277d2e627856868374ce104039fc4490f8
charlinegrard-coder <charline.grard@mmibordeaux.com>
2026-09-23 10:26:58 +0200

    Update database.php

---
+++ config/database.php
@@ -0,0 +1,26 @@
+<?php
+
+$host = "localhost";
+$port = "8889";
+$dbname = "budgetsync";
+$user = "root";
+$pass = "root";
+
+try {
+
+    $pdo = new PDO(
+        "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8",
+        $user,
+        $pass
+    );
+
+    $pdo->setAttribute(
+        PDO::ATTR_ERRMODE,
+        PDO::ERRMODE_EXCEPTION
+    );
+
+} catch(PDOException $e) {
+
+    die("Erreur : " . $e->getMessage());
+}
+?>
